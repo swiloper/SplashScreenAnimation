db3ab8e0d3a6510ec75f526d7c2bd4b5898a7f83
Ihor Myronishyn <myronishun.ihor@icloud.com>
2022-05-07 00:59:59 +0300

    Initial Commit

---
+++ Splash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Splash
+//
+//  Created by myronishyn.ihor on 07.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
